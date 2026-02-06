8f436d2bf6e9660cd393074dc0f5fb81b060f301
Jose M. Gomez <gomezjosem@live.com>
2026-02-06 16:23:48 +0000

    show use of collections in go

---
+++ data/data.go
@@ -0,0 +1,14 @@
+package data
+
+// Example of Arrays
+var Numbers = [5]int{1, 2, 3, 4, 5}
+
+// Example of Slices
+var Fruits = []string{"Apple", "Banana", "Cherry"}
+
+// Example of Maps
+var Capitals = map[string]string{
+	"France":  "Paris",
+	"Germany": "Berlin",
+	"Japan":   "Tokyo",
+}
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"collections/data"
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("Hello, World!")
+	fmt.Println(data.Capitals)
+	fmt.Println(data.Fruits)
+	fmt.Println(data.Numbers)
+
+}
