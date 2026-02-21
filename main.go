76b84f86638b4c235e45dfbe353647580fed8277
Jose M. Gomez <gomezjosem@live.com>
2026-02-21 15:13:33 +0000

    Implement concurrent currency rate fetching using goroutines and WaitGroups

--- main.go
+++ main.go
@@ -3,10 +3,35 @@ package main
 import (
 	"api_client/api"
 	"fmt"
+	"sync"
 )
 
 func main() {
-	rate, err := api.GetRate("BTC")
+	currencies := []string{"BTC", "ETH", "BCH"}
+	var wg sync.WaitGroup
+
+	for _, currency := range currencies {
+		wg.Add(1) // Increment WaitGroup counter
+		go func() {
+			/*
+				from Go v1.22 onwards,
+				for loop variables have per-iteration scope,
+				so there is no need to make a copy of the variable for currency.
+				In prior version of Go, the currency variable would be shared
+				across each go routine thread,
+				leading to the final value of currency being evaluated in all Go routines,
+				it was a source of common bugs in Go code.
+			*/
+			getCurrencyRate(currency)
+			wg.Done()
+		}()
+	}
+	wg.Wait() // Wait for all goroutines to finish
+
+}
+
+func getCurrencyRate(currency string) {
+	rate, err := api.GetRate(currency)
 	if err != nil {
 		panic(err)
 	}
