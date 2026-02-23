95f6630b4eaf2532075a3fca7d008bb96cb6f6a5
Jose M. Gomez <gomezjosem@live.com>
2026-02-23 22:54:47 +0000

    demonstrate go templates for server side rendering

--- main.go
+++ main.go
@@ -1,14 +1,35 @@
 package main
 
-import "net/http"
+import (
+	"http_server/data"
+	"net/http"
+	"text/template"
+)
 
 func handleHello(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from a Go program"))
 }
 
+func handleTemplate(w http.ResponseWriter, r *http.Request) {
+	html, err := template.ParseFiles("templates/index.tmpl")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Error parsing template"))
+		return
+	}
+	err = html.Execute(w, data.GetAll())
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Error executing template"))
+		return
+	}
+
+}
+
 func main() {
 	server := http.NewServeMux()
 	server.HandleFunc("/hello", handleHello)
+	server.HandleFunc("/template", handleTemplate)
 
 	fs := http.FileServer(http.Dir("./public"))
 	server.Handle("/", fs)
